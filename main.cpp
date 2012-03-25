6311691982ab2d02d886edb372375fd29511413a
wangyc03 <wangyc03@gmail.com>
2012-03-25 15:23:24 +0000

    auc=0.7

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
-//#include "source.cpp"
 #include <stdio.h>
-#include <dlib/matrix.h>
-#include "rep.h"
-//#include "naive_bay.h"
-using namespace dlib;
+#include "naive_bay.h"
 
 int main()
 {
-//	NaiveBay().run();
-	demo_ad();
-/*
-	parse();
-	split();
-*/
-/*
-	matrix<double,2,2> m;
-	set_all_elements(m,1.3);
-	m = m*m;
-	printf("hello dlib %f\n", m(0,1));
-*/
+	NaiveBay().run();
 	return 0;
 }
