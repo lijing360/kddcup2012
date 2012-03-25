a4097ab875c38052c40e526f136d874279b770cb
wangyc03@gmail.com <wangyc03@gmail.com@7d331129-4b52-415e-9f0a-e58efa67a64d>
2012-03-25 11:12:00 +0000

    0.0871874 0.260341

---
+++ trunk/main.cpp
@@ -0,0 +1,23 @@
+//#include "source.cpp"
+#include <stdio.h>
+#include <dlib/matrix.h>
+#include "rep.h"
+//#include "naive_bay.h"
+using namespace dlib;
+
+int main()
+{
+//	NaiveBay().run();
+	demo_ad();
+/*
+	parse();
+	split();
+*/
+/*
+	matrix<double,2,2> m;
+	set_all_elements(m,1.3);
+	m = m*m;
+	printf("hello dlib %f\n", m(0,1));
+*/
+	return 0;
+}
